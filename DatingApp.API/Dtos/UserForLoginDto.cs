65b7eff7d29ef52e60d1828bdf166969ab43407d
Nazar <nazar121099@gmail.com>
2019-12-28 17:27:31 -0800

    Repo. pattern and Server side token auth

---
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public System.String Username { get; set; }
+        public System.String Password { get; set; }
+    }
+}
